72449505f903944699da1ea2911271421280b29a
Bombo <bombo96@chungwoon.ac.kr>
2023-07-05 22:22:38 +0900

    feat : Point가 0 ~ 24 사이의 좌표를 입력받도록 구현

---
+++ src/main/java/domain/Point.java
@@ -0,0 +1,29 @@
+package domain;
+
+public class Point {
+
+    private final int MIN_RANGE_VALUE = 0;
+    private final int MAX_RANGE_VALUE = 24;
+
+    private int x;
+    private int y;
+
+    public Point(int x, int y) {
+        validatePointRange(x, y);
+        this.x = x;
+        this.y = y;
+    }
+
+    private void validatePointRange(int x, int y) {
+        if (axisOutOfRange(x) || axisOutOfRange(y)) {
+            throw new IllegalArgumentException("x와 y의 좌표는 0이상 24이하여야 합니다.");
+        }
+    }
+
+    private boolean axisOutOfRange(int axis) {
+        if (axis < MIN_RANGE_VALUE || axis > MAX_RANGE_VALUE) {
+            return true;
+        }
+        return false;
+    }
+}
---
+++ src/test/java/domain/PointTest.java
@@ -0,0 +1,28 @@
+package domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PointTest {
+
+    @ParameterizedTest
+    @CsvSource(value = {"-1:24", "0:25", "25:0", "24:-1"}, delimiter = ':')
+    @DisplayName("x와 y좌표는 0미만 24초과이면 예외가 발생한다.")
+    void failCreatePoint(int x, int y) {
+        Assertions.assertThatThrownBy(() -> new Point(x, y))
+            .hasMessageContaining("x와 y의 좌표는 0이상 24이하여야 합니다.");
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"0:24", "24:24", "24:0", "0:0"}, delimiter = ':')
+    @DisplayName("x와 y좌표는 0이상 24이하이면 정상적으로 생성된다.")
+    void CreatePoint(int x, int y) {
+        Assertions.assertThatCode(() -> new Point(x, y))
+                .doesNotThrowAnyException();
+    }
+}
