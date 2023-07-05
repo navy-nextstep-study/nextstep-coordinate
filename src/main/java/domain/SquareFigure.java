dd3cd04fc7749d04ecef9b1899c236f7e283f4c9
Bombo <bombo96@chungwoon.ac.kr>
2023-07-06 01:13:40 +0900

    feat : 직사각형을 판별하고, 사각형의 면적을 구하는 기능 추가

--- src/main/java/domain/SquareFigure.java
+++ src/main/java/domain/SquareFigure.java
@@ -1,5 +1,9 @@
 package domain;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 public class SquareFigure implements Figure {
 
     private final Points points;
@@ -10,6 +14,54 @@ public class SquareFigure implements Figure {
 
     @Override
     public Double calculate() {
-        return null;
+        if (!isRectangle()) {
+            throw new IllegalArgumentException("직사각형이 아닙니다.");
+        }
+
+        return getArea();
+    }
+
+    private Double getArea() {
+        List<Point> pointList = points.getPoints();
+        Point pointA = pointList.get(0);
+        Point pointB = pointList.get(1);
+        Point pointC = pointList.get(2);
+        Point pointD = pointList.get(3);
+
+        List<Double> pointLengthList = new ArrayList<>(List.of(
+            pointA.calculateDistance(pointB),
+            pointA.calculateDistance(pointC),
+            pointA.calculateDistance(pointD),
+            pointB.calculateDistance(pointC),
+            pointB.calculateDistance(pointD),
+            pointC.calculateDistance(pointD)));
+
+        pointLengthList.sort(Comparator.naturalOrder());
+
+        return pointLengthList.get(0) * pointLengthList.get(2);
+    }
+
+
+    private boolean isRectangle() {
+        List<Point> pointList = points.getPoints();
+        Point pointA = pointList.get(0);
+        Point pointB = pointList.get(1);
+        Point pointC = pointList.get(2);
+        Point pointD = pointList.get(3);
+
+        List<Double> pointLengthList = new ArrayList<>(List.of(
+            pointA.calculateDistance(pointB),
+            pointA.calculateDistance(pointC),
+            pointA.calculateDistance(pointD),
+            pointB.calculateDistance(pointC),
+            pointB.calculateDistance(pointD),
+            pointC.calculateDistance(pointD)));
+
+        pointLengthList.sort(Comparator.naturalOrder());
+
+        return pointLengthList.get(0).equals(pointLengthList.get(1))
+            && pointLengthList.get(2).equals(pointLengthList.get(3))
+            && pointLengthList.get(4).equals(pointLengthList.get(5))
+            && pointLengthList.get(2) < pointLengthList.get(4);
     }
 }
