da7e446c77adf6f1b4fd853586dc3e68db5b3aef
Yiseull <omjl5123@catholic.ac.kr>
2023-07-05 15:28:47 +0900

    feat: 좌표 추가

---
+++ src/main/java/com/nextstep/coordinate/model/Coordinate.java
@@ -0,0 +1,17 @@
+package com.nextstep.coordinate.model;
+
+public class Coordinate {
+
+    private final int value;
+
+    public Coordinate(int value) {
+        validationValue(value);
+        this.value = value;
+    }
+
+    private void validationValue(int value) {
+        if (0 > value || value > 24) {
+            throw new IllegalArgumentException("좌표는 0 이상 24 이하의 값만 가질 수 있습니다.");
+        }
+    }
+}
