b222762f006bedf2c21f79d9b29a043ea1e80c87
Yiseull <omjl5123@catholic.ac.kr>
2023-07-05 16:42:08 +0900

    refactor: Point 생성자 매개변수 타입 변경

--- src/main/java/com/nextstep/coordinate/model/Point.java
+++ src/main/java/com/nextstep/coordinate/model/Point.java
@@ -7,9 +7,9 @@ public class Point {
     private final Coordinate x;
     private final Coordinate y;
 
-    public Point(Coordinate x, Coordinate y) {
-        this.x = x;
-        this.y = y;
+    public Point(int x, int y) {
+        this.x = new Coordinate(x);
+        this.y = new Coordinate(y);
     }
 
     public double calculateDistance(Point point) {
--- src/test/java/com/nextstep/coordinate/model/PointTest.java
+++ src/test/java/com/nextstep/coordinate/model/PointTest.java
@@ -1,33 +1,21 @@
 package com.nextstep.coordinate.model;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.offset;
 
 public class PointTest {
-
-    @Test
-    @DisplayName("X, Y 좌표를 갖는다.")
-    void hasXAndYCoordinates() {
-        // given
-        Coordinate x = new Coordinate(1);
-        Coordinate y = new Coordinate(1);
-
-        // when & then
-        assertThatCode(() -> new Point(x, y))
-                .doesNotThrowAnyException();
-    }
 
     @ParameterizedTest
     @CsvSource(value = {"1, 1.414", "2, 2.828"})
     @DisplayName("점과 점의 거리를 계산한다.")
     void calculateDistance(int value, double expected) {
         // given
-        Point point1 = new Point(new Coordinate(0), new Coordinate(0));
-        Point point2 = new Point(new Coordinate(value), new Coordinate(value));
+        Point point1 = new Point(0, 0);
+        Point point2 = new Point(value, value);
 
         // when
         double result = point1.calculateDistance(point2);
